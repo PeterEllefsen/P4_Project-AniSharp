c0504693905cd78dd7dc82ca991f3dd255488520
PeterEllefsen <pellef@live.dk>
2023-04-12 11:32:05 +0200

    Visitor intitial setup
    Parser and Lexer for the language have been generated as ANTLR recognizers, and C# classes for these have been created. The AnimationLanguageVisitor class have been created as a subclass to AnimationLanguageBaseVisitor.

---
+++ AST/AnimationLanguage/AnimationLanguageVisitor.cs
@@ -0,0 +1,22 @@
+﻿using AnimationLanguage;
+namespace AnimationLanguage;
+
+//Vi benytter os af return typen 'object' for base visitoren, da den repræsenterer alle former for returtyper vi kan støde på
+//Den kan også returnere null, da ting som assignment ikke returnerer noget.
+public class AnimationVisitor: AnimationLanguageRulesBaseVisitor<object?>
+{
+    private Dictionary<string, object?> Variables = new();
+
+    //Dette er hvad der sker når vi besøger en assignment operation
+    public override object? VisitAssignment(AnimationLanguageRulesParser.AssignmentContext context)
+    {
+        //Vi tildeler de værdier vi har fået fra vores context til tilsvarende variable i vores kode.
+        var varType = context.type().GetText();
+        var varName = context.IDENTIFIER().First().GetText();
+        var value = context.expression().GetText();
+
+        Variables[varName] = value;
+        
+        return null;
+    }
+}
---
+++ AST/AnimationLanguage/Class1.cs
@@ -0,0 +1,19 @@
+﻿using AnimationLanguage;
+using Antlr4.Runtime;
+
+//Setup for læsning af ExampleCode.ss
+
+var fileName = "Content\\ExampleCode.ss";
+
+var fileContent = File.ReadAllText(fileName);
+
+var inputStream = new AntlrInputStream(fileContent);
+
+var animationLanguageRulesLexer = new AnimationLanguageRulesLexer(inputStream);
+var commonTokenStream = new CommonTokenStream(animationLanguageRulesLexer);
+var animationLanguageParser = new AnimationLanguageRulesParser(commonTokenStream); //Der kan ved denne skrives .AddErrorListener(new DiagnosticErrorListener()) for at få fejl i consolen
+var animationLanguageContext = animationLanguageParser.program();
+var visitor = new AnimationVisitor();        
+visitor.Visit(animationLanguageContext);
+
+var i = 1;
