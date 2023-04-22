38c748eb8c665f642454b155bb7ebbf5ca45ed9d
PeterEllefsen <pellef@live.dk>
2023-04-22 21:32:39 +0200

    I've created a custom ExpressionVisitor, and overridden all of the expression cases

--- AST/AnimationLanguage/Visitor/AnimationLanguageVisitor.cs
+++ AST/AnimationLanguage/Visitor/AnimationLanguageVisitor.cs
@@ -106,7 +106,7 @@ public class AnimationLanguageVisitor : AnimationLanguageRulesBaseVisitor<IASTNo
         return keyValuePairNode;
     }
     
-    
+    //VisitTerminal works to create an IdentifierNode when the parser visits an IDENTIFIER terminal node.
     public override IASTNode VisitTerminal(ITerminalNode node)
     {
         if (node.Symbol.Type == AnimationLanguageRulesParser.IDENTIFIER)
@@ -121,14 +121,14 @@ public class AnimationLanguageVisitor : AnimationLanguageRulesBaseVisitor<IASTNo
     //This method visits an assignment context and returns an AssignmentNode.
     public override IASTNode VisitAssignment(AnimationLanguageRulesParser.AssignmentContext context)
     {
-        AssignmentOperator assignmentOperator = VisitAssOps(context.assOps()); // Visit the assignment operator context and get the current AssignmentOperator.
-        IdentifierNode identifierNode = (IdentifierNode)VisitTerminal((ITerminalNode)context.GetChild(0)); // Visit the IDENTIFIER context and get the IdentifierNode.
-        ExpressionNode expression = (ExpressionNode)Visit(context.GetChild(2)); // Visit the expression context and get the ExpressionNode.
+        AssignmentOperator assignmentOperator = VisitAssOps(context.assOps());
+        IdentifierNode identifierNode = (IdentifierNode)VisitTerminal((ITerminalNode)context.GetChild(0));
+        ExpressionNode expression = (ExpressionNode)VisitExpression(context.expression()); // Call the custom VisitExpression method
 
-        //Create a new AssignmentNode with the retrieved identifier, assignment operator and expression data.
         AssignmentNode assignmentNode = new AssignmentNode(identifierNode, assignmentOperator, expression, GetSourceLocation(context.Start));
         return assignmentNode;
     }
+
 
     //This method visits the assignment operator context and returns the current AssignmentOperator.
     public AssignmentOperator VisitAssOps(AnimationLanguageRulesParser.AssOpsContext context)
@@ -152,11 +152,176 @@ public class AnimationLanguageVisitor : AnimationLanguageRulesBaseVisitor<IASTNo
     }
 
 
+    //This method visits a type context and returns a TypeNode based on what type the context is.
+    public override IASTNode VisitType(AnimationLanguageRulesParser.TypeContext context)
+    {
+        TypeNode.TypeKind typeKind; //The TypeKind enum is used to determine what types are allowed.
+
+        // Check the type of the context and set the corresponding TypeKind.
+        if (context.INT() != null)
+        {
+            typeKind = TypeNode.TypeKind.Int;
+        }
+        else if (context.FLOAT_TYPE() != null)
+        {
+            typeKind = TypeNode.TypeKind.Float;
+        }
+        else if (context.STRING_TYPE() != null)
+        {
+            typeKind = TypeNode.TypeKind.String;
+        }
+        else if (context.BOOL() != null)
+        {
+            typeKind = TypeNode.TypeKind.Bool;
+        }
+        else if (context.CIRCLE() != null)
+        {
+            typeKind = TypeNode.TypeKind.Circle;
+        }
+        else if (context.POLYGON() != null)
+        {
+            typeKind = TypeNode.TypeKind.Polygon;
+        }
+        else
+        {
+            throw new NotSupportedException($"Unsupported type encountered at line {context.Start.Line} column {context.Start.Column}");
+        }
+
+        // Create a new TypeNode with the determined TypeKind and SourceLocation.
+        TypeNode typeNode = new TypeNode(typeKind, GetSourceLocation(context.Start));
+
+        return typeNode;
+    }
+
+
+    //This is a custom method that visits an expression context and returns an ExpressionNode. Since it is not present in the parser, it does not need to be overridden.
+    public IASTNode VisitExpression(AnimationLanguageRulesParser.ExpressionContext context)
+    {
+        if (context is AnimationLanguageRulesParser.IntegerExpressionContext)
+        {
+            return VisitIntegerExpression((AnimationLanguageRulesParser.IntegerExpressionContext)context); //Visit the IntegerExpressionContext and return the result.
+        }
+        else if (context is AnimationLanguageRulesParser.FloatExpressionContext)
+        {
+            return VisitFloatExpression((AnimationLanguageRulesParser.FloatExpressionContext)context); //Visit the FloatExpressionContext and return the result.
+        }
+        else if (context is AnimationLanguageRulesParser.StringExpressionContext)
+        {
+            return VisitStringExpression((AnimationLanguageRulesParser.StringExpressionContext)context); //Visit the BooleanExpressionContext and return the result.
+        }
+        else if (context is AnimationLanguageRulesParser.BooleanExpressionContext)
+        {
+            return VisitBooleanExpression((AnimationLanguageRulesParser.BooleanExpressionContext)context); //Visit the BooleanExpressionContext and return the result.
+        }
+        else if (context is AnimationLanguageRulesParser.BinaryExpressionContext)
+        {
+            return VisitBinaryExpression((AnimationLanguageRulesParser.BinaryExpressionContext)context); //Visit the BinaryExpressionContext and return the result.
+        }
+        else if (context is AnimationLanguageRulesParser.IdentifierExpressionContext)
+        {
+            ITerminalNode identifierNode = ((AnimationLanguageRulesParser.IdentifierExpressionContext)context).IDENTIFIER(); //Store the IDENTIFIER terminal in a variable.
+            return VisitTerminal(identifierNode); // Call VisitTerminal with the IDENTIFIER terminal
+        }
+        else if (context is AnimationLanguageRulesParser.FunctionCallExpressionContext)
+        {
+            AnimationLanguageRulesParser.FuncCallContext funcCallContext = ((AnimationLanguageRulesParser.FunctionCallExpressionContext)context).funcCall(); //Store the funcCall context in a variable.
+            return VisitFuncCall(funcCallContext); // Call VisitFuncCall with the funcCall context.
+        }
+        else if (context is AnimationLanguageRulesParser.ShapeInitExpressionContext)
+        {
+            AnimationLanguageRulesParser.ShapeinitContext shapeInitContext = ((AnimationLanguageRulesParser.ShapeInitExpressionContext)context).shapeinit(); //Store the shapeInit context in a variable.
+            return VisitShapeinit(shapeInitContext); // Call VisitShapeinit with the shapeInit context.
+        }
+        else if (context is AnimationLanguageRulesParser.TermExpressionContext)
+        {
+            AnimationLanguageRulesParser.TermContext termContext = ((AnimationLanguageRulesParser.TermExpressionContext)context).term(); //Store the term context in a variable.
+            return VisitTerm(termContext); // Call VisitTerm with the term context.
+        }
+        else
+        {
+            throw new InvalidOperationException($"Unexpected expression type in VisitExpression at {GetSourceLocation(context.Start)}");
+        }
+    }
+
+    public override IASTNode VisitIntegerExpression(AnimationLanguageRulesParser.IntegerExpressionContext context)
+    {
+        int value = int.Parse(context.INTEGER().GetText()); //Store the value of the context in a variable.
+        SourceLocation sourceLocation = GetSourceLocation(context.Start);
+        return new IntegerLiteralNode(value, sourceLocation); //Create a new IntegerLiteralNode with the value and SourceLocation.
+    }
+
+    public override IASTNode VisitFloatExpression(AnimationLanguageRulesParser.FloatExpressionContext context)
+    {
+        float value = float.Parse(context.FLOAT().GetText());
+        SourceLocation sourceLocation = GetSourceLocation(context.Start);
+        return new FloatLiteralNode(value, sourceLocation);
+    }
+
+    public override IASTNode VisitStringExpression(AnimationLanguageRulesParser.StringExpressionContext context)
+    {
+        string value = context.STRING().GetText();
+        value = value.Substring(1, value.Length - 2); // Removes the quotes surrounding the string, suchh that it is only the relevant data we gather
+        SourceLocation sourceLocation = GetSourceLocation(context.Start);
+        return new StringLiteralNode(value, sourceLocation); 
+    }
+
+    public override IASTNode VisitBooleanExpression(AnimationLanguageRulesParser.BooleanExpressionContext context)
+    {
+        bool value = context.boolean().TRUE() != null;
+        SourceLocation sourceLocation = GetSourceLocation(context.Start);
+        return new BooleanLiteralNode(value, sourceLocation);
+    }
+
+    //This method is called when a function call is encountered in the code.
+    public override IASTNode VisitFuncCall(AnimationLanguageRulesParser.FuncCallContext context)
+    {
+        IdentifierNode functionName = (IdentifierNode)VisitTerminal((ITerminalNode)context.IDENTIFIER()); //Visit the IDENTIFIER terminal and cast the result to an IdentifierNode.
+        List<ArgumentNode> arguments = new List<ArgumentNode>(); //Create a list of ArgumentNodes to store the arguments of the function call.
+
+        if (context.call_parameters() != null) //If the function call has arguments, visit them and add them to the list.
+        { 
+            int numParameters = context.call_parameters().ChildCount; //ChildCountt represents the number of arguments in a function call
+            for (int i = 0; i < numParameters; i++)
+            {
+                if (context.call_parameters().GetChild(i) is AnimationLanguageRulesParser.Call_parameterContext parameterContext) //If the child is a call_parameter context, visit it and add it to the list.
+                {
+                    arguments.Add((ArgumentNode)Visit(parameterContext));
+                }
+            }
+        }
+        SourceLocation sourceLocation = GetSourceLocation(context.Start);
+        return new FunctionCallNode(functionName, arguments, sourceLocation);
+    }
+
+
+    //This represents a shape initialization.
+    public override IASTNode VisitShapeinit(AnimationLanguageRulesParser.ShapeinitContext context)
+    {
+        //If the shape is a circle, create a new TypeNode with the TypeKind Circle, otherwise create a new TypeNode with the TypeKind Polygon.
+        TypeNode shapeType = context.CIRCLE() != null ? new TypeNode(TypeNode.TypeKind.Circle, GetSourceLocation(context.CIRCLE().Symbol)) : new TypeNode(TypeNode.TypeKind.Polygon, GetSourceLocation(context.POLYGON().Symbol)); 
+        Dictionary<string, IASTNode> arguments = new Dictionary<string, IASTNode>(); //Create a dictionary to store the arguments of the shape initialization for either a circle or a polygon.
+
+        for (int i = 0; i < context.argName().Length; i++)
+        {
+            string argName = context.argName(i).GetText().TrimEnd(':'); //Get the name of the argument and remove the colon at the end.
+            IASTNode argValue = Visit(context.arg(i)); //Visit the argument and store the value of the argument in a variable.
+            arguments.Add(argName, argValue);
+        }
+
+        SourceLocation sourceLocation = GetSourceLocation(context.Start);
+        return new ShapeInitNode(shapeType, arguments, sourceLocation);
+    }
+
+    public override IASTNode VisitTerm(AnimationLanguageRulesParser.TermContext context)
+    {
+        IASTNode innerExpression = Visit(context.GetChild(1));
+        return innerExpression;
+    }
 
 
 
 
-
+    
     //---------------------------Helper methods---------------------------//
     private SourceLocation GetSourceLocation(IToken token)
     {
