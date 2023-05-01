a648a02d2f08d834ccc6f431b953328511048385
PeterEllefsen <pellef@live.dk>
2023-05-01 14:16:03 +0200

    Fixed return node not containing identifiers

--- AST/AnimationLanguage/ASTNodes/IndentifierNode.cs
+++ AST/AnimationLanguage/ASTNodes/IndentifierNode.cs
@@ -1,26 +1,21 @@
 ﻿namespace AnimationLanguage.ASTNodes;
 using ASTCommon;
 
-public class IdentifierNode : IASTNode
+public class IdentifierNode : ExpressionNode
 {
-    public SourceLocation SourceLocation { get; set; }
-    public NodeType NodeType => NodeType.Identifier;
-
     public string Name { get; set; }
 
     public IdentifierNode(string name, SourceLocation sourceLocation)
+        : base(ExpressionNodeType.Identifier, null, null, null, sourceLocation)
     {
         Name = name;
-        SourceLocation = sourceLocation;
     }
-    
-    
+
     public IEnumerable<IASTNode> GetChildren()
     {
         return Enumerable.Empty<IASTNode>(); // Identifiers have no children, so it returns an empty list.
     }
-    
-    
+
     public override string ToString()
     {
         return $"IdentifierNode: {Name}";
