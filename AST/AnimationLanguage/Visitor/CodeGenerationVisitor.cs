59ae158de921a5b6aecfe326648a6f8f5e8fb9ca
Daniel <Daniel@canvy.com>
2023-05-17 14:20:45 +0200

    Write to file

--- AST/AnimationLanguage/Visitor/CodeGenerationVisitor.cs
+++ AST/AnimationLanguage/Visitor/CodeGenerationVisitor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Threading.Channels;
 using AnimationLanguage.ASTNodes;
+using systemio = System.IO;
 
 namespace AnimationLanguage.Visitor;
 using ASTCommon;
@@ -8,277 +9,261 @@ using ASTCommon;
 
 public class CodeGenerationVisitor : ASTVisitor<IASTNode>
 {
-    /*
-    private readonly SymbolTable _symbolTable;
-    */
+    private void CreateFilesForCompilation() {
+
+        //files to create
+        //Program.cs for main
+        //function class for all functions
+        //Sequence class containing methods that create sequences
+        using (FileStream fs = File.Create("../../codegen/Program.cs", 1024))
+        {
+        }
+        using (FileStream fs = File.Create("../../codegen/Functions.cs", 1024))
+        {
+        }
+        using (FileStream fs = File.Create("../../codegen/Sequences.cs", 1024))
+        {
+        }
+    }
+
+    private void AppendToFile(string file, string appendingString) {
+        string path = "";
+        if (file == "main") {
+            path = "../../../codegen/Program.cs";
+        }
+
+        if (path != "") {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(appendingString);
+            }	
+        }
+
+    }
+
     public CodeGenerationVisitor()
     {
     }
 
     public override IASTNode? Visit(ProgramNode node)
     {
-        Console.WriteLine("Type checking program node: ");
-        foreach (var child in node.GetChildren()) {
-            Console.WriteLine(child);
-        }
+        AppendToFile("main", "indsæt kode her");
+        
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(SetupNode node)
     {
-        Console.WriteLine("Type checking setup node: ");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(GroupingElementsNode node)
     {
-        Console.WriteLine("Type checking groupingelements node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(KeyValuePairNode node)
     {
-        Console.WriteLine("Type checking key value pair node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(IdentifierNode node)
     {
-        Console.WriteLine("Type checking identifier node");
         return VisitChildren(node);
     }
 
     public override IASTNode? Visit(AssignmentNode node)
     {
-        Console.WriteLine("Type checking assignment node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(OperatorNode node)
     {
-        Console.WriteLine("Type checking operator node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(IdentifierGroupingNode node)
     {
-        Console.WriteLine("Type checking identifier grouping node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(ArgumentNode node)
     {
-        Console.WriteLine("Type checking argument node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(FunctionDeclarationNode node)
     {
-        Console.WriteLine("Type checking function declaration node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(FunctionCallNode node)
     {
-        Console.WriteLine("Type checking function call node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(ParameterNode node)
     {
-        Console.WriteLine("Type checking parameter node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(CallParameterNode node)
     {
-        Console.WriteLine("Type checking call parameter node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(PrototypeNode node)
     {
-        Console.WriteLine("Type checking prototype node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(StatementNode node)
     {
-        Console.WriteLine("Type checking statement node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(IfStatementNode node)
     {
-        Console.WriteLine("Type checking if statement node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(ElseIfNode node)
     {
-        Console.WriteLine("Type checking else if node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(ElseNode node)
     {
-        Console.WriteLine("Type checking else node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(WhileLoopNode node)
     {
-        Console.WriteLine("Type checking while statement node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(ForLoopNode node)
     {
-        Console.WriteLine("Type checking for loop node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(BlockNode node)
     {
-        Console.WriteLine("Type checking block node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(SeqBlockNode node)
     {
-        Console.WriteLine("Type checking SeqBlock node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(AnimationNode node)
     {
-        Console.WriteLine("Type checking declaration node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(CommandNode node)
     {
-        Console.WriteLine("Type checking command node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(ConditionNode node)
     {
-        Console.WriteLine("Type checking condition node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(ExpressionNode node)
     {
-        Console.WriteLine("Type checking expression node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(FrameDefNode node)
     {
-        Console.WriteLine("Type checking framedef node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(IntegerLiteralNode node)
     {
-        Console.WriteLine("Type checking integer literal node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(FloatLiteralNode node)
     {
-        Console.WriteLine("Type checking float literal node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(StringLiteralNode node)
     {
-        Console.WriteLine("Type checking string literal node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(BooleanLiteralNode node)
     {
-        Console.WriteLine("Type checking boolean literal node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(ReturnNode node)
     {
-        Console.WriteLine("Type checking return node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(SeqBlockPartNode node)
     {
-        Console.WriteLine("Type checking seqblockpart node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(SequenceCallNode node)
     {
-        Console.WriteLine("Type checking sequence node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(SequenceNode node)
     {
-        Console.WriteLine("Type checking sequence node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(PolygonNode node)
     {
-        Console.WriteLine("Type checking polygon node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(CircleNode node)
     {
-        Console.WriteLine("Type checking circle node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(TimelineBlockNode node)
     {
-        Console.WriteLine("Type checking TimelineBlock node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(TransitionNode node)
     {
-        Console.WriteLine("Type checking Transition node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(TupleNode node)
     {
-        Console.WriteLine("Type checking Tuple node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(TypeNode node)
     {
-        Console.WriteLine("Type checking Type node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(UnaryOperationNode node)
     {
-        Console.WriteLine("Type checking UnaryOperation node");
         return VisitChildren(node);
     }
     
     public override IASTNode? Visit(NodeList<IASTNode> node)
     {
-        Console.WriteLine("Type checking VariableDeclaration node");
         return VisitChildren(node);
     }
 
-        public override IASTNode? Visit(ShapeInitNode node)
+    public override IASTNode? Visit(ShapeInitNode node)
     {
-        Console.WriteLine("Type checking Shapeinit");
         return VisitChildren(node);
     }
 }
